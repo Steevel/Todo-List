65825862ed1d46dfc35c7851fc981879816399d9
Steevel <57296025+Steevel@users.noreply.github.com>
2023-02-19 11:51:14 +0530

    Page reload upon clicking logo functionality added

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -4,10 +4,14 @@ import CreateTodo from './CreateTodo'
 import TodoList from './TodoList'
 
 const Todo = () => {
+    function reloadPage() {
+        window.location.reload()
+    }
+
     return (
         <div className='text-white flex flex-col h-full w-full'>
             <div className="">
-                <img src={Logo} alt="Logo" />
+                <img src={Logo} alt="Logo" onClick={() => reloadPage()} />
             </div>
             <CreateTodo />
             <TodoList />
