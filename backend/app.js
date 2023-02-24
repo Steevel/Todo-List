4362a9e94b79710ec755074a63e9abd89463ba52
Steevel <57296025+Steevel@users.noreply.github.com>
2023-02-24 18:14:58 +0530

    Server app.js updated

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors());
 
 // Connect to the database
 connectToDB();
--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:4000/api";
+// export const BASE_URL = "http://localhost:4000/api";
+export const BASE_URL = "https://tasklist-rv2y.onrender.com";
